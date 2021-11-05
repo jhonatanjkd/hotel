85a46268808331931928d02f1727e498e9dd4fe9
jhonatanjkd <47799433+jhonatanjkd@users.noreply.github.com>
2021-11-04 21:37:12 -0500

    segundo commit

---
+++ index.php
@@ -0,0 +1,320 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <meta property="og:title" content="">
+  <meta property="og:type" content="">
+  <meta property="og:url" content="">
+  <meta property="og:image" content="">
+
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="apple-touch-icon" href="icon.png">
+  <!-- Place favicon.ico in the root directory -->
+
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="assets/bootstrap-5.0.2-dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/main.css">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
+
+  <meta name="theme-color" content="#fafafa">
+</head>
+
+<body>
+
+  <!-- Add your site or application content here -->
+
+
+  <header class="header">
+    <div class="headerTop d-none d-md-block">
+      <div class="container">
+        <div class="row">
+          <div class="col-4 col-md-5">
+            <div class="row align-items-start">
+              <div class="col col-sm-8">Terminos y condiciones</div>
+              <div class="col col-sm-4">US</div>
+            </div>
+          </div>
+          <div class="col"></div>
+          <div class="col-4 col-md-5">
+            <div class="row align-items-start">
+              <div class="col">My bookings</div>
+              <div class="col">Ad Your review</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="menu">
+      <div class="">
+        <nav>
+          <div class="hamburger">
+            <div class="line"></div>
+            <div class="line"></div>
+            <div class="line"></div>
+          </div>
+          <div class="container">
+            <div class="row">
+              <div class="col-3">
+                <div class="logo"><a><img src="img/logotipo.png"></a></div>
+              </div>
+              <div class="col-9">
+                <ul class="nav-links">
+                  <li><a href="#">Home</a></li>
+                  <li><a href="#">Our History</a></li>
+                  <li><a href="#">Rooms</a></li>
+                  <li><a href="#">Beach Club</a></li>
+                  <li><a href="#">Restaurant</a></li>
+                  <li><a href="#">Contact Us</a></li>
+                 <!--  <li><a href="#">Tours</a></li> -->
+                </ul>
+              </div>
+            </div>
+          </div>
+
+        </nav>
+      </div>
+    </div>
+  </header>
+  <div class="bannerHome">
+    <div class="textoBanner">
+      <div class="centroTexto">
+        <span>LUXUY HOTEL & BEST RESORT</span>
+        <div>X</div>
+        <h1>ENJOY A LUXURY EXPERIENCE</h1>
+        <a>ROOMS E SUITES</a>
+      </div>
+    </div>
+  </div>
+  <div class="container">
+    <section class="roomsHome">
+      <h2>OUR ROOMS</h2>
+      <span>-PROMOTIONS-</span>
+      <div class="row">
+        <div class="col-12 col-sm-6">
+          <div class="itemRoomsHome">
+            <a>
+              <img src="img/home/IMG_6662.jpg" alt="" />
+              <div class="typeRoomsHome">
+                <span>SMALL ROOM</span>
+                <span>FROM 30 $</span>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="col-12 col-sm-6">
+          <div class="itemRoomsHome">
+            <a>
+              <img src="img/home/IMG_6629.jpg" alt="" />
+              <div class="typeRoomsHome">
+                <span>Room with View</span>
+                <span>FROM 30 $</span>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="col-12 col-sm-6">
+          <div class="itemRoomsHome">
+            <a>
+              <img src="img/home/IMG_02632.jpg" alt="" />
+              <div class="typeRoomsHome">
+                <span>Apartment</span>
+                <span>FROM 30 $</span>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="col-12 col-sm-6">
+          <div class="itemRoomsHome">
+            <a>
+              <img src="img/home/IMG_6678.jpg" alt="" />
+              <div class="typeRoomsHome">
+                <span>Luxury Room</span>
+                <span>FROM 30 $</span>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="infoGeneral">
+      <div class="row">
+        <div class="col-12 col-lg-4">
+          <h3>Hello. Our hotel has been present for over <u>20 years</u> . We make the best for all !</h3>
+        </div>
+        <div class="col-12 col-lg-4">
+          <div class="fotoInfo">
+            <img src="img/home/IMG_5543.jpg" alt="">
+            <div class="textoItemG">
+              <span>LOREM IPSUM</span>
+              <span>SIT AMET</span>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-lg-4">
+          <div class="fotoInfo">
+            <img src="img/home/IMG_2988.jpg" alt="">
+            <div class="textoItemG">
+              <span>LOREM IPSUM</span>
+              <span>SIT AMET</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="MoreItems">
+      <div class="row">
+        <div class="col-12 col-sm-6">
+          <div class="itemsIconos">
+            <img src="http://www.nicdarkthemes.com/themes/resort/wp/demo/hotel/wp-content/uploads/sites/3/2019/05/icon01.png" alt="">
+            <div class="notaItem">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean egestas magna at porttitor vehicula nullam augue ipsum dolor.</p>
+              <a href="#">READ MORE</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-sm-6">
+          <div class="itemsIconos">
+            <img src="http://www.nicdarkthemes.com/themes/resort/wp/demo/hotel/wp-content/uploads/sites/3/2019/05/icon02.png" alt="">
+            <div class="notaItem">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean egestas magna at porttitor vehicula nullam augue ipsum dolor.</p>
+              <a href="#">READ MORE</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-sm-6">
+          <div class="itemsIconos">
+            <img src="http://www.nicdarkthemes.com/themes/resort/wp/demo/hotel/wp-content/uploads/sites/3/2019/05/icon03.png" alt="">
+            <div class="notaItem">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean egestas magna at porttitor vehicula nullam augue ipsum dolor.</p>
+              <a href="#">READ MORE</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-sm-6">
+          <div class="itemsIconos">
+            <img src="http://www.nicdarkthemes.com/themes/resort/wp/demo/hotel/wp-content/uploads/sites/3/2019/05/icon04.png" alt="">
+            <div class="notaItem">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean egestas magna at porttitor vehicula nullam augue ipsum dolor.</p>
+              <a href="#">READ MORE</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+  <section class="descubreUbicacion">
+    <div class="division">
+      <div class="divisionImagen">
+        <img src="img/home/IMG_6584.jpg" />
+      </div>
+    </div>
+    <div class="division">
+      <div class="divisionTexto">
+        <h4>LUXURY HOTEL & BEST RESORT</h4>
+        <h3>Discover our <br><span>Locations</span></h3>
+        <div class="divTextos">
+          <p>Phasellus enim libero, blandit vel sapien vitae, condimentum ultricies magna et. Quisque euismod orci ut et lobortis. Blandit vel sapien vitae, condimentum ultricies magna et orci ut et lobortis, Phasellus enim</p>
+          <p>Phasellus enim libero, blandit vel sapien vitae, condimentum ultricies magna et. Quisque euismod orci ut et lobortis. Blandit vel sapien vitae, condimentum ultricies magna et orci ut et lobortis, Phasellus enim</p>
+
+        </div>
+        <div class="btnRoomsText">ROOMS E SUITS</div>
+      </div>
+    </div>
+  </section>
+  <footer class="footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <ul class="listFooter">
+            <li><span>Main Contacts</span></li>
+            <li>Asburd 14 (Miami)</li>
+            <li>info@resort.com</li>
+            <li>+23 3453776547</li>
+            <li>Reception 24 H</li>
+          </ul>
+        </div>
+        <div class="col-md-4">
+          <ul class="listFooter">
+            <li><span>Site Links</span></li>
+            <li>ABOUT US</li>
+            <li>OUR SERVICES</li>
+            <li>OUR ROOMS</li>
+            <li>PAYMENTS</li>
+          </ul>
+        </div>
+        <div class="col-md-4">
+          <div class="SocialRedes">
+            <span>Social</span>
+            <ul class="social">
+              <li><a><i class="fab fa-google-plus-g"></i></a></li>
+              <li><a><i class="fab fa-twitter"></i></a></li>
+              <li><a><i class="fab fa-instagram"></i></a></li>
+              <li><a><i class="fab fa-facebook-f"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="cintafooter">
+      <div class="container">
+        <div class="row">
+          <div class="col-4">
+            <small>Terms of Use · Privacy Policy</small>
+          </div>
+          <div class="col-4">
+            <small>Copyright 2021 Nicdark.com</small>
+          </div>
+          <div class="col-4">
+            <div class="iconpagos">
+              <i class="fab fa-cc-visa"></i>
+              <i class="fab fa-cc-mastercard"></i>
+              <i class="fab fa-cc-paypal"></i>
+              <i class="fab fa-cc-amex"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+
+
+  <script src="js/vendor/modernizr-3.11.2.min.js"></script>
+  <script src="js/plugins.js"></script>
+  <script src="js/main.js"></script>
+
+  <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+  <script>
+    window.ga = function() {
+      ga.q.push(arguments)
+    };
+    ga.q = [];
+    ga.l = +new Date;
+    ga('create', 'UA-XXXXX-Y', 'auto');
+    ga('set', 'anonymizeIp', true);
+    ga('set', 'transport', 'beacon');
+    ga('send', 'pageview')
+  </script>
+  <script src="https://www.google-analytics.com/analytics.js" async></script>
+
+
+  <script>
+    const hamburger = document.querySelector(".hamburger");
+    const navLinks = document.querySelector(".nav-links");
+    const links = document.querySelectorAll(".nav-links li");
+
+    hamburger.addEventListener("click", () => {
+      navLinks.classList.toggle("open");
+      links.forEach(link => {
+        link.classList.toggle("fade");
+      });
+    });
+  </script>
+</body>
+
+</html>
